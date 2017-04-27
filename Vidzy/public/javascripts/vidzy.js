9dba06ffcb27702033e37ca0a1522cadb2750d2f
Godfrey Chiu <godfrey.chiu@hssmi.org>
2017-04-27 12:56:43 +0100

    Added Angular, with POST form for adding new videos, next step to implement the controller

---
+++ Vidzy/public/javascripts/vidzy.js
@@ -0,0 +1,24 @@
+var app = angular.module('Vidzy', ['ngRoute', 'ngResource']);
+
+app.config(['$routeProvider', function($routeProvider){
+	$routeProvider
+	.when('/', {
+		templateUrl: 'partials/home.html',
+		controller: 'HomeCtrl'
+	})
+	.when('/add-video', {
+		templateUrl: 'partials/video-form.html'
+	})
+	.otherwise({
+		redirectTo: '/'
+	});
+}]);
+
+app.controller('HomeCtrl', ['$scope', '$resource',
+	function($scope, $resource){
+		var Videos = $resource('/api/videos');
+		Videos.query(function(videos){
+			$scope.videos = videos;
+		})
+
+}]);
